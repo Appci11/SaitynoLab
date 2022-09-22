e79c582a254781b9767f882190595483ef95b5d4
Appci11 <61198490+Appci11@users.noreply.github.com>
2022-09-22 20:53:02 +0300

    Locally (in variable) stored User. Auth api controller (register, login). Jwt token creation.

---
+++ SaitynoLab/Shared/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaitynoLab.Shared
+{
+    public class User
+    {
+        public int Id { get; set; } = default!; // esant duombazei default nuimt
+        public string Username { get; set; } = string.Empty;
+        //public string Password { get; set; } = string.Empty;
+
+        //Saziningesnis variantas...
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}

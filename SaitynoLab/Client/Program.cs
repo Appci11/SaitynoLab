ce3c9cd3ff60991917f0cdf8770d695ddadae6b2
Appci11 <61198490+Appci11@users.noreply.github.com>
2022-11-16 18:31:30 +0200

    dfgh

--- SaitynoLab/Client/Program.cs
+++ SaitynoLab/Client/Program.cs
@@ -2,10 +2,11 @@ global using Microsoft.AspNetCore.Components.Authorization;
 global using SaitynoLab.Client.Services.UsersService;
 global using SaitynoLab.Shared;
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SaitynoLab.Client;
-
+using SaitynoLab.Client.Policies;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,6 +16,12 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
+
+builder.Services.AddAuthorizationCore(config =>
+{
+    config.AddPolicy(Policies.IsAdmin, Policies.IsAdminPolicy());
+    config.AddPolicy(Policies.IsUser, Policies.IsUserPolicy());
+});
 
 builder.Services.AddScoped<IUsersService, UsersService>();
 
--- SaitynoLab/Server/Services/AuthService/AuthService.cs
+++ SaitynoLab/Server/Services/AuthService/AuthService.cs
@@ -58,7 +58,8 @@ namespace SaitynoLab.Server.Services.AuthService
             List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.Role, user.Role)
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("id", user.Id.ToString())
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
